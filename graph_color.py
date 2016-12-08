8eea3839b36ccddb7d0d60f3ec424deee0e7165b
Aaron <aaronknobloch92@gmail.com>
2016-12-07 21:02:38 -0500

    Adds enum types for Graph colors

---
+++ graph_color.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class GraphColor(Enum) :
+	
+	background = "#f5f5f0"
+	
+	visited_green = "#99ff99" 
+	alive_green = "#009933" 
+	
+	visited_blue = "#33ccff" 
+	alive_blue = "#0033cc" 

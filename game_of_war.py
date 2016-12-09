864e567a32248111633afd27f1cc9b85ac79678c
Aaron <aaronknobloch92@gmail.com>
2016-12-08 21:50:53 -0500

    Adds radio buttons, Issue #5

--- game_of_war.py
+++ game_of_war.py
@@ -4,25 +4,10 @@ from graph import GraphWidget
 from graph_color import GraphColor
 from game_logic import GameLogic
 import time
-
-def select_green(graph_view, blueb, greenb) :
-
-	graph_view.set_color(GraphColor.alive_green)
-	greenb.state(["disabled"])
-	blueb.state(["!disabled"])
-	
-	
-def select_blue(graph_view, blueb, greenb) :
-	
-	graph_view.set_color(GraphColor.alive_blue)
-	greenb.state(["!disabled"])
-	blueb.state(["disabled"])
 	
 def start(startb, blueb, greenb, canvas, runner) :
 	
 	canvas.paint_enabled = False
-	greenb.state(["disabled"])
-	blueb.state(["disabled"])
 	
 	# if game is not running
 	if(not runner.running) :
@@ -48,7 +33,7 @@ main_frame.grid(column = 1, row = 3)
 game = GameLogic()
 
 # calculate screen size to determine game board size
-screen_width = root.winfo_screenwidth() * 1/2  # game board coveres 3/4 of screen
+screen_width = root.winfo_screenwidth() * 1/2  # game board covers 1/2 of screen
 screen_height = root.winfo_screenheight() * 1/2
 square_pixel_size = 10
 
@@ -67,13 +52,16 @@ label_group.grid(column = 1, row = 2)
 # Add color selection label
 ttk.Label(label_group, text = "Place Civilization: ").grid(column = 1, row = 1)
 
-# Add green color selection button
-green_button = ttk.Button(label_group, text = "Green", command = lambda: select_green(my_graph, blue_button, green_button))
-green_button.grid(column = 2, row = 1)
-green_button.state(["disabled"])
+# Create variable to allow green to be default selected
+v = IntVar()
+v.set(1) # 1 is green radio button
 
-# Add blue color selection button
-blue_button = ttk.Button(label_group, text = "Blue", command = lambda: select_blue(my_graph, blue_button, green_button))
+# Add green color radio button
+green_button = ttk.Radiobutton(label_group, text = "Green", variable = v, command = lambda: my_graph.set_color(GraphColor.alive_green), value = 1)
+green_button.grid(column = 2, row = 1)
+
+# Add blue color radio button
+blue_button = ttk.Radiobutton(label_group, text = "Blue", variable = v, command = lambda: my_graph.set_color(GraphColor.alive_blue), value = 2)
 blue_button.grid(column = 3, row = 1)
 
 # Add start button
